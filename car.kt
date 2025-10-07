b220f18699a216f62ea36ae5199355b35ff1d272
marcocampos13 <macamposto@est.utn.ac.cr>
2025-10-06 20:22:25 -0600

    Dessarollo del primer método en curso

--- car.kt
+++ car.kt
@@ -1,10 +1,24 @@
-interface IVehicle{  
-  fun start_engine() //print a message the vehicle was started and the acceleration is 0
-  fun accelerate(acceleration: Int) //print a message the current acceleration of the vehicle, you need to sum the new acceleration
-  fun brake() //print a message the vehicle is stopped and the acceleration is 0
-  fun turn_off_engine() //print a message the vehicle was turned off with acceleration = 0
+//Este es el primer método
+
+interface IVehicle {
+    fun start_engine()
 }
 
-class Car: IVehicle{
-  
+class Car : IVehicle {
+    private var encender: String = ""
+    private var aceleracion: Int = 0
+
+    constructor(encender: String, aceleracion: Int) {
+        this.encender = encender
+        this.aceleracion = aceleracion
+    }
+
+    override fun start_engine() {
+        println("El carro se está encendiendo...")
+        println("El carro está $encender y la aceleración es $aceleracion")
+    }
+}
+
+fun main() {
+
 }
