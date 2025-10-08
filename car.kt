fc539e1630f07e5a15dd75c60a6c06c7c249bf3a
Kelunie <caleb.cordero1997@gmail.com>
2025-10-08 13:48:32 -0600

    The US #1 is done

--- car.kt
+++ car.kt
@@ -6,5 +6,25 @@ interface IVehicle{
 }
 
 class Car: IVehicle{
-  
+
+  // Caleb does the start?start_engine function
+  overrride fun start_engine(){
+    acceleration = 0
+    println("The vehicle was started and the acceleration is $acceleration")
+  }
+
+  // *name* does the accelerate function
+  overrride fun accelerate(){
+
+  }
+
+  // *name* does the accelerate function
+  overrride fun brake(){
+    
+  }
+
+  // *name* does the accelerate function
+  overrride fun turn_off_engine(){
+    
+  }
 }
